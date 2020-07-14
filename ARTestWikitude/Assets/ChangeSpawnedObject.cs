2dca66c2a80f93d603eea1af9d19e4cc130f9b61
Nick Wang <nickwang1.29.7@gmail.com>
2020-07-14 21:45:02 +0200

    Import project

---
+++ ARTestWikitude/Assets/ChangeSpawnedObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Wikitude;
+
+public class ChangeSpawnedObject : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _objects = new List<GameObject>();
+
+    public void OnObjectRecognized(ObjectTarget recognizedObject)
+    {
+        foreach (GameObject o in _objects)
+            if (o.name == recognizedObject.Name)
+            {
+                GameObject g = Instantiate(o, transform);
+                g.transform.parent = recognizedObject.Drawable.transform;
+            }
+    }
+
+    public void TrackTest(ImageTarget recognizedObject)
+    {
+        Debug.LogError($"[Unity] {recognizedObject.Name}");
+    }
+}
---
+++ ARTestWikitude/Assets/TrackerToggle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Wikitude;
+
+public class TrackerToggle : SampleController
+{
+    public ObjectTracker OTracker;
+    public ImageTracker ITracker;
+
+    private bool _waitingForTrackerToLoad = false;
+
+    public void OnOTracker() {
+        if (!OTracker.enabled && !_waitingForTrackerToLoad) {
+            _waitingForTrackerToLoad = true;
+
+            OTracker.enabled = true;
+        }
+    }
+
+    public void OnITracker() {
+        if (!ITracker.enabled && !_waitingForTrackerToLoad) {
+            _waitingForTrackerToLoad = true;
+
+            ITracker.enabled = true;
+        }
+    }
+
+    public override void OnTargetsLoaded() {
+        _waitingForTrackerToLoad = false;
+    }
+}
